6ad5b3c06d725f577da7a731bf87a69146b5936c
Wahylian <rido1607@gmail.com>
2026-06-18 19:21:13 +0300

    switched to a new better dataset, with 20k pictures

--- downloaddataset.py
+++
@@ -1,8 +0,0 @@
-import kagglehub
-import os
-
-os.environ["KAGGLEHUB_CACHE"] = os.path.dirname(os.path.abspath(__file__))
-
-path = kagglehub.dataset_download("chuneeb/deepfake-detection-dataset-2026")
-
-print("Path to dataset files:", path)
